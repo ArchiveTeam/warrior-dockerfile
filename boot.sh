30b752891203a1c3a711b0b62c4bdd1b33cd0b0d
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-02-07 03:49:34 +0100

    tee the boot output to follow along

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /home/warrior/warrior-code2
-sudo -u warrior DOCKER=1 ./boot.sh >>/var/log/warrior-boot.log 2>&1
+sudo -u warrior DOCKER=1 ./boot.sh 2>&1 | sudo tee /var/log/warrior-boot.log
