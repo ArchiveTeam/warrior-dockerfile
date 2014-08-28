133f4a5eb7c89071bf83133e0085b1ce68b6ac34
Steve Phillips <steve@tryingtobeawesome.com>
2014-08-27 22:04:23 -0400

    Added TERM=dumb to ./boot.sh call
    Fixes https://github.com/ArchiveTeam/warrior-dockerfile/issues/5

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /home/warrior/warrior-code2
-sudo -u warrior DOCKER=1 ./boot.sh 2>&1 | sudo tee /var/log/warrior-boot.log
+sudo -u warrior DOCKER=1 TERM=dumb ./boot.sh 2>&1 | sudo tee /var/log/warrior-boot.log
