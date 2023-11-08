ArchiveTeam/warrior-dockerfile

commit b46ba0a05e16e7106ee4019726e2fdf23fc74331

Version 20230910.01. Temporary hack to get sudo to work.

Dockerfile
@@ -1,9 +1,15 @@
 FROM atdr.meo.ws/archiveteam/grab-base
 
-LABEL version="20230727.01"
+LABEL version="20230910.01"
 
 RUN apt-get update \
  && apt-get install -y --no-install-recommends nodejs \
+ && rm -rf /var/lib/apt/lists/*
+
+# temporary hack
+RUN rm /usr/bin/sudo \
+ && apt-get update \
+ && apt-get install -y --no-install-recommends sudo \
  && rm -rf /var/lib/apt/lists/*
 
 RUN useradd -m warrior --uid 1000 \

commit 62ee62887d777d12a324d428b0ac1da1cda1e0c6

Version 20231108.01. Remove broken temporary hack introduced in b46ba0a05e16e7106ee4019726e2fdf23fc74331
With the recent addition of sudo in grab-base, the manual removal of /usr/bin/sudo breaks sudo for all further commands.

Dockerfile
@@ -1,15 +1,9 @@
 FROM atdr.meo.ws/archiveteam/grab-base
 
-LABEL version="20231107.01"
+LABEL version="20231107.02"
 
 RUN apt-get update \
  && apt-get install -y --no-install-recommends nodejs \
- && rm -rf /var/lib/apt/lists/*
-
-# temporary hack
-RUN rm /usr/bin/sudo \
- && apt-get update \
- && apt-get install -y --no-install-recommends sudo \
  && rm -rf /var/lib/apt/lists/*
 
 RUN useradd -m warrior --uid 1000 \